374786b4069788c71b5ce1ec8ff71b7a4fe252ec
Morten Ledum <mortele@student.matnat.uio.no>
2016-09-21 10:08:55 +0200

    First commit.

---
+++ tfpotential.py
@@ -0,0 +1,8 @@
+import os
+import sys
+import shutil 
+import tensorflow 			as 		tf
+import numpy 				as 		np
+import matplotlib.pyplot 	as 		plt
+import generateData 		as 		gen
+import datetime 			as 		time
