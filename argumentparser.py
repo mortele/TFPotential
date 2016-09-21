721d173e6818f2050b64ca0c92bd5ca45d6e63ef
Morten Ledum <mortele@student.matnat.uio.no>
2016-09-21 11:00:28 +0200

    Added an argument parser for the TFPotential class.

---
+++ argumentparser.py
@@ -0,0 +1,18 @@
+import argparse as arg
+
+class ArgumentParser :
+	def __init__(self) :
+		description = 'Fit potential energy surfaces using tensorflow neural networks.'
+		self.parser = arg.ArgumentParser(description=description)
+		self.parser.add_argument('epochs', help='The number of training epochs',\
+								 type=int, default=-1, nargs='?')
+		self.parser.add_argument('--plot', help='Plot the network potential and the error',\
+								 default=False, action='store_true')
+		self.parser.add_argument('--load', help='Load graph from previous training file',\
+								 default=None)
+		self.parser.add_argument('--save', help='Save the graph and training data to file',\
+								 default=False, action='store_true')
+		self.args = self.parser.parse_args()
+
+	def __call__(self) :
+		return self.args
--- tfpotential.py
+++ tfpotential.py
@@ -1,8 +1,20 @@
 import os
 import sys
 import shutil 
-import tensorflow 			as 		tf
-import numpy 				as 		np
-import matplotlib.pyplot 	as 		plt
-import generateData 		as 		gen
-import datetime 			as 		time
+import tensorflow 			as 	tf
+import numpy 				as 	np
+import matplotlib.pyplot 	as 	plt
+import datetime 			as 	time
+import argumentparser 		as 	ap
+
+
+
+class TFPotential :
+	def __init__(self) :
+		self.argumentParser = ap.ArgumentParser()
+
+	def __call__(self) :
+		pass
+
+
+tfpot = TFPotential()
